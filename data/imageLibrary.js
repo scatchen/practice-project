fe34564fb6783dd56b752996206370cee2b1ca25
Sky Chen <scatchen5@gmail.com>
2026-07-03 14:36:09 +0800

    fix: diagramMap image path bug + improve image rendering

---
+++ data/imageLibrary.js
@@ -0,0 +1,14 @@
+export const imageLibrary = {
+    geometry: [
+        {
+      id: "triangle-exterior-angle",
+      file: "images/geometry/triangle-exterior-angle.png",
+      title: "三角形外角",
+    },
+     {
+      id: "pythagorean",
+      file: "images/geometry/pythagorean.svg",
+      title: "畢氏定理",
+    },
+    ],
+};
